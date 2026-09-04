0758e894afbf428e84610f36d2bd2611e797d5a4
Steve Traylen <steve.traylen@cern.ch>
2026-09-04 10:44:00 +0200

    Support Debian 13, EL10 and Fedora 44
    Support:
    
    * Debian 13
    * Fedora 44
    * Rocky 10
    * RedHat 10
    * AlmaLinux 10
    
    Debian 13 and Ubuntu 26.04 (once supported) will deploy with deb822 apt configuration.

--- spec/support/acceptance/prepare_host.rb
+++ spec/support/acceptance/prepare_host.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 def prepare_host
-  shell('yum install -y epel-release') if fact('os.family') == 'RedHat'
+  return unless %w[RedHat Rocky AlmaLinux CentOS OracleLinux].include?(fact('os.name'))
+
+  package = if fact('os.name') == 'OracleLinux' && fact('os.release.major').to_i >= 10
+              "oracle-epel-release-el#{fact('os.release.major')}"
+            else
+              'epel-release'
+            end
+
+  shell("yum install -y #{package}")
 end
